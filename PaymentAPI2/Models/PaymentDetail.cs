49dbf47b1d9b566c9e684a481b8108940d75afb8
Enes AYDIN <89972814+enesaydiinn@users.noreply.github.com>
2021-11-10 10:17:35 +0300

    Add project files.

---
+++ PaymentAPI2/Models/PaymentDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI2.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName="nvarChar(100)")]
+        public string CardOwnerName { get; set; }
+        [Column(TypeName = "nvarChar(16)")]
+        public string CardNumber { get; set; }
+        [Column(TypeName = "nvarChar(5)")]
+        public string ExpirationDate { get; set; }
+        [Column(TypeName = "nvarChar(3)")]
+        public string SecurityCode { get; set; }
+
+
+    }
+}
